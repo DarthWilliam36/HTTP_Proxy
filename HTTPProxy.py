10152e2f5bd7cf1867c6c7f955e2723c20ce78c5
William Martin <will@Williams-MacBook-Air.local>
2023-05-18 19:55:48 -0400

    Update Proxy to support client

--- HTTPProxy.py
+++ HTTPProxy.py
@@ -8,18 +8,23 @@ class MyRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         # Handle GET requests
         print("headers: " + str(self.headers))
+
+        if self.headers.get("secret-connect"):
+            target_address = self.headers["secret-connect"]
+            self.connect_relay(target_address)
+            return
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
     def do_CONNECT(self):
-        self.connect_relay()
-
-    def connect_relay(self):
         address = self.path.split(':', 1)
-        address[1] = int(address[1]) or 443
+        self.connect_relay(address)
+
+    def connect_relay(self, target_address):
         try:
-            s = socket.create_connection(address, timeout=self.timeout)
+            s = socket.create_connection(target_address, timeout=self.timeout)
         except:
             self.send_error(502)
             return
