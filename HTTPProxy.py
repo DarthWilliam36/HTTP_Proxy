9e24f7d832177cb615a30ce31836125fd4ce97ad
William Martin <will@Williams-MacBook-Air.local>
2023-05-18 20:53:16 -0400

    More Bug Fixxxx`

--- HTTPProxy.py
+++ HTTPProxy.py
@@ -11,7 +11,7 @@ class MyRequestHandler(BaseHTTPRequestHandler, CustomProxy):
 
         if self.headers.get("secret-connect"):
             target_address = self.headers["secret-connect"]
-            target_address = target_address.split(":")
+            target_address = tuple(target_address.split(":"))
             print("target address:" + str(target_address))
             s = self.try_connect(target_address)
             self.connect_relay(s)
